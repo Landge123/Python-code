353a6f87fa745c5c146d53751d0edfb980822343
Landge123 <95701171+Landge123@users.noreply.github.com>
2023-03-07 22:23:23 +0530

    Add files via upload

---
+++ practical6.3.py
@@ -0,0 +1,6 @@
+a = [18, 52, 23, 41, 32]
+
+greatest = max(a)
+
+
+print(f'Greatest number in the list is : {greatest}.')
---
+++ practical6.4.py
@@ -0,0 +1,5 @@
+a = [18, 52, 23, 41, 32]
+
+smallest = min(a)
+
+print(f'Smallest number in the list is : {smallest}.')
