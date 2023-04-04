0cd339acbccb8fab92be0aee433bd361ac5f46a6
Landge123 <95701171+Landge123@users.noreply.github.com>
2023-04-04 20:09:12 +0530

    Update Practical8.1.py

--- Practical8.1.py
+++ Practical8.1.py
@@ -3,6 +3,6 @@ Rollno = {112, 114, 116, 118, 115}
 print('Rollno',Rollno)
 Rollno.add(111)
 print('Rollno after adding element',Rollno)
-Rollno.add(112)
+Rollno.remove(112)
 print('Rollno after removing element',Rollno)
 
