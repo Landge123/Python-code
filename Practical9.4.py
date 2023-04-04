76ef7d62f438b9f78fab144c5d4d31820e939dc6
Landge123 <95701171+Landge123@users.noreply.github.com>
2023-04-04 20:42:58 +0530

    Update Practical9.4.py

--- Practical9.4.py
+++ Practical9.4.py
@@ -1,4 +1,4 @@
 L = [{"V":"S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII":"S005"}, {"V":"S009"},{"VIII":"S007"}]
 print("List: ",L)
-u_value = set( val for dic in L for val in dic.values())
-print("Unique Values: ",u_value)
+value = set( val for dic in L for val in dic.values())
+print("Unique Values: ",value)
